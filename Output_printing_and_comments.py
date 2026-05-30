21bd42448f23826a7b20c3bc00162b24487eca2f
Nazib-personal <nazibahrar.official@gmail.com>
2026-05-30 06:09:28 +0600

    Initial commit for Output printing and comments

---
+++ Output_printing_and_comments.py
@@ -0,0 +1,34 @@
+
+
+print('hello World')  #Outputs whatever's inside print function
+
+
+print("1st line")
+print("2nd line")  #Prints outputs line after line sequentially
+
+
+print("I am",22)  #Comma separated print keeps a space between the datas (if they're different data types)
+
+
+print("Nazib"+"is"+"a"+"chill"+"guy")  #prints strings one after another
+
+
+print("1st line")
+print()
+print("2nd line")  #prints a gap between 1st and 2nd line
+
+
+print("1st line",end=" ")
+print("2nd line")           #instead of starting from a new line 2nd line immediately sits next to 1st line with a space between them for end=" "
+
+
+print("Hello\nbro")  #prints the string after \n to a newline
+
+
+print("A\\B\\C")
+print("A\\B\C")   # Backlash always counts the character next to it as a string
+
+
+print("Tell him I said,'Hello'")  #quote inside a quote
+
+
